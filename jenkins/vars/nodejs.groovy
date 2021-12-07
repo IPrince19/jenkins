918284ac2c9a51310d7f10770239f2ca1a3357f2
prince.igbineweka <Prince.Igbineweka@gmail.com>
2021-12-07 12:25:39 -0700

    Nodejs.groovy file in Jenkins.

--- jenkins/vars/nodejs.groovy
+++ jenkins/vars/nodejs.groovy
@@ -1,2 +1,7 @@
-stage('Code Quality') {
+tage('Code Quality') {
+    steps {
+        sh """
+sonar-scanner -Dsonar.projectKey=sample -Dsonar.sources=. -Dsonar.host.url=http://172.31.7.240:9000 -Dsonar.login=c07b0b5bd58bb2c1b8768e2d6339dadfd4e77894
+       """
+    }
 }
