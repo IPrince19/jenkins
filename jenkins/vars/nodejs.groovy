e51ca44a08108d44fe47b3df6183d23b8a53de81
prince.igbineweka <Prince.Igbineweka@gmail.com>
2021-12-07 14:45:22 -0700

    Modified Nodejs.groovy code in Jenkins.

--- jenkins/vars/nodejs.groovy
+++ jenkins/vars/nodejs.groovy
@@ -1,4 +1,4 @@
-tage('Code Quality') {
+stage('Code Quality') {
     steps {
         sh """
 sonar-scanner -Dsonar.projectKey=sample -Dsonar.sources=. -Dsonar.host.url=http://172.31.7.240:9000 -Dsonar.login=c07b0b5bd58bb2c1b8768e2d6339dadfd4e77894
